8522521e9bc62f8782e133a5b8e7d9c474d6c754
Abdullah Saif <bosaif39tm@gmail.com>
2024-09-08 19:23:51 +0400

    Update day_7_project_hangman.py

--- day_7_project_hangman.py
+++ day_7_project_hangman.py
@@ -1,16 +1,12 @@
 import random
 
-#Words for the game
 words = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(words)
-print(f"Chosen word is {chosen_word}")
-last = len(chosen_word)
-
-my_list = ['_'] * last
+wordLen = len(chosen_word)
+my_list = ['_'] * wordLen
 print(my_list)
 
-#Number of lives the player has
 lives = 6
 
 #List to keep track of guessed letters
@@ -19,32 +15,28 @@ guess_list = []
 #Main game loop: continues until the player has guessed all letters or runs out of lives
 
 while '_' in my_list:
-    #Display the number of lives remaining
-    print(f"You have {lives} left")
-    
+    print(f"You have {lives} lives left")
     guess = input("Guess a letter: ").lower()
     
-    # Check if the guessed letter is in the chosen word
-    for i in range(last):
+    for i in range(wordLen):
         if chosen_word[i] == guess:
 
             my_list[i] = guess
             print(my_list)
 
-            if '_' not in my_list:
+            if ('_' not in my_list):
                 print("You win")
                 break  
     
-    if guess in guess_list:
+    if (guess in guess_list):
         print(f"You have used the letter {guess}")
     
     
-    elif guess not in chosen_word:
+    elif (guess not in chosen_word):
         print(f"The letter {guess} is not in the word")
         guess_list.append(guess)  
         lives -= 1
         
-        #Check if the player has run out of lives
-        if lives == 0:
+        if (lives == 0):
             print("You lose")
             break  
